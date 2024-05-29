658ce823834f400458a59c26fb39d2cc9390fb0e
Hamza Samad <160117284+Hamzasamad55@users.noreply.github.com>
2024-05-29 17:36:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+#! usr/bin/env node
+// string in typescript
+let fruits = "Orange";
+console.log(fruits);
+fruits = "Mango";
+console.log(fruits);
+fruits = "Apple";
+console.log(fruits);
+// agar ham " " k andar number bh likhen gen tw woh bh strign kehlaega
+// for example,
+let HamzaNum = "1222";
+console.log(HamzaNum);
+export {};
---
+++ index.ts
@@ -0,0 +1,18 @@
+#! usr/bin/env node
+
+// string in typescript
+
+let fruits: string = "Orange";
+console.log(fruits);
+
+fruits = "Mango";
+console.log(fruits);
+
+fruits = "Apple";
+console.log(fruits);
+
+// agar ham " " k andar number bh likhen gen tw woh bh strign kehlaega
+// for example,
+
+let HamzaNum = "1222";
+console.log(HamzaNum);
